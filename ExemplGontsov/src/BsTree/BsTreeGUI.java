ed1e433371df6e894ff93a33ce570725718ed216
nickhammerk20 <kolich.x10@gmail.com>
2016-06-18 15:11:22 +0300

    tree.GUI - done!

--- ExemplGontsov/src/BsTree/BsTreeGUI.java
+++ ExemplGontsov/src/BsTree/BsTreeGUI.java
@@ -12,35 +12,29 @@ public class BsTreeGUI extends BsTree
 	{
 		if (p == null)
 			return;
-		
+
 		/*Vertex = START */
 		int x = (left+right)/2;
 		int y = dy * level;
 		grap.drawString("" + p.val, x+7, y+20);	
 		grap.drawOval(x, y, 30, 30);
-		
+
 		if (p.left != null)
 		{
-			int xLine = ( (right + left)/2 );
-			int yLine = ( dy * ( level - 1 ) + 30);
-			grap.drawLine(x+15, y+30, xLine, yLine);
+			int xLine = ( (left + x)/2+15 );
+			int yLine = ( dy * ( level + 1 ));
+			grap.drawLine(xLine, yLine, x+15, y+30);
 		}
-//		if (level != 1)
-//		{
-//			if(p.val < 25)
-//			{
-//			int yLine = ( dy * ( level - 1 ) + 30);
-//			int xLine = ( (right - left)+ 15 );
-//			System.out.println("p.val= " + p.val + ", xLine= " + xLine+ ", yLine= "  +  yLine + ", x= " + x + ", y= " +  y);
-//			grap.drawLine(xLine, yLine, x+14, y);
-//			}
-//		}				
-		//System.out.print(p.val + ",");
+		if (p.right != null)
+		{
+			int xLine = ( (right + x)/2+15 );
+			int yLine = ( dy * ( level + 1 ));
+			grap.drawLine(xLine, yLine, x+15, y+30);
+		}
 		/*Vertex = END  */
-			
+
 		printNodeGUI(p.left, 	grap, 	left , 	x , 	level+1,	dy);          		//Left
 		printNodeGUI(p.right,	grap,  	x , 	right, 	level+1, 	dy);             	//Right
 	}
 
 }
-  
