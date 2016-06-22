21a35cfc1c4f9ce35ea65db3aca05a24670b324e
nickhammerk20 <kolich.x10@gmail.com>
2016-06-22 17:58:27 +0300

    HomeWork 22/06/16_4

--- PaintRastr/src/RightButton.java
+++ PaintRastr/src/RightButton.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.MenuItem;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
@@ -8,8 +9,32 @@ public class RightButton extends JPanel
 {
 	public RightButton(int x, int y) 
 	{
+		setLocation(x, y);
 		JTextField cmenu = new JTextField();
-		setBounds(70, 0, 1110, 540);
-		setBackground(Color.WHITE);
+		
+		MenuItem miSave = new MenuItem("Save");
+		MenuItem miOpen = new MenuItem("Open");
+		MenuItem miClear = new MenuItem("Clear");
+		MenuItem mw2 = new MenuItem("2");
+		MenuItem mw3 = new MenuItem("3");
+		MenuItem mw4 = new MenuItem("4");
+
+		MenuItem mcRed = new MenuItem("RED");
+		MenuItem mcGreen = new MenuItem("GREEN");
+		MenuItem mcBlue = new MenuItem("BLUE");
+		
+//		cmenu.add(miOpen);
+//		cmenu.add(miSave);
+//		cmenu.add("-");
+//		cmenu.add(miClear);
+//		cmenu.add(mw2);
+//		cmenu.add(mw3);
+//		cmenu.add(mw4);
+//		cmenu.add(mcRed);
+//		cmenu.add(mcGreen);
+//		cmenu.add(mcBlue);
+//		cmenu.add(mFile);
+//		cmenu.add(mWidth);
+//		cmenu.add(mColor);
 	}
 }
--- PaintRastr/src/XZPanel.java
+++ PaintRastr/src/XZPanel.java
@@ -1,15 +1,18 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 
 import javax.swing.BorderFactory;
-import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JPopupMenu;
+import javax.swing.SwingUtilities;
 
 public class XZPanel extends JPanel implements MouseListener, MouseMotionListener
 {
@@ -17,8 +20,11 @@ public class XZPanel extends JPanel implements MouseListener, MouseMotionListene
 	int x = 0;
 	int y = 0;
 	
+	JPopupMenu popup;
+	JLabel label;
+
 	BufferedImage bi = new BufferedImage(1200, 600, BufferedImage.TYPE_INT_ARGB);
-	
+
 	public XZPanel() 
 	{
 		setLayout(null);
@@ -28,23 +34,29 @@ public class XZPanel extends JPanel implements MouseListener, MouseMotionListene
 		addMouseListener(this);
 		addMouseMotionListener(this);
 	}
-	
+
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
 		Graphics2D gg = (Graphics2D) g;
 		gg.drawImage(bi, 0, 0, null);
 	}
-	
+
 	@Override
 	public void mouseClicked(MouseEvent e) 
 	{
-		if (e.getButton() == MouseEvent.BUTTON3)
-		{
-			RightButton p = new RightButton(e.getX(), e.getY());
-			add(p);		
-			repaint();
-		}		
+		JPanel panel = new JPanel();
+		panel.add(label);
+		popup = new JPopupMenu();
+		popup.add(new JMenuItem("open"));
+		popup.add(new JMenuItem("save"));
+		label.addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent event) {
+				if (SwingUtilities.isRightMouseButton(event))
+					popup.show(label, event.getX(), event.getY());
+			}
+		});
+		setContentPane(panel);	
 	}
 	@Override
 	public void mousePressed(MouseEvent e) 
