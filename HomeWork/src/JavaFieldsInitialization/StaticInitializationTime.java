7b96784ac430332c669a06b761c964642df06c89
nickhammerk20 <kolich.x10@gmail.com>
2016-07-08 23:33:30 +0300

    HW 08.07.16

---
+++ HomeWork/src/JavaFieldsInitialization/StaticInitializationTime.java
@@ -0,0 +1,19 @@
+package JavaFieldsInitialization;
+
+
+
+public class StaticInitializationTime {
+    public static class C {
+        static int i = value();
+        static int value() {
+            System.out.println("C.i initialized");
+            return 1;
+        }
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        System.out.println("Before class loading");
+      //  Class.forName(C.class.getName());
+        System.out.println("After class loading");
+    }
+}
